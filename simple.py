9d335f24b84f5f4e5465e386d5fe4423973a3c05
lingzhuharbin <32422794+lingzhuharbin@users.noreply.github.com>
2017-09-30 17:13:13 -0700

    edit2

--- simple.py
+++ simple.py
@@ -9,3 +9,14 @@ while (x < 10):
 else :
      print('The sum of first 9 integers : ',s)        
 print('The sum of ',x,' numbers is :',s) 
+
+# break in for loop
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
+num_sum = 0
+count = 0
+for x in numbers:
+    num_sum = num_sum + x
+    count = count + 1 
+    if count == 5:
+        break
+print("Sum of first ",count,"integers is : ", num_sum)
